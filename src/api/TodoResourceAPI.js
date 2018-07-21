a59cd73e8634617e41140b51b2a7330075caad0c
quinnHuang <quinn.huang@oocl.com>
2018-07-21 17:43:09 +0800

    [Quinn] delete api

--- src/api/TodoResourceAPI.js
+++
@@ -1,9 +0,0 @@
-const TodoAPI={
-    const todos=[{id:111,text:"小猪佩奇",completed:false}]
-    toggleActive=(id)={
-        const todo=todos.find(t=>t.id==id)
-        todo.completed=!todo.completed
-        return todo
-    }
-}
-export default TodoAPI
